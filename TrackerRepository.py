4177a9e8ccfa9901bb2d5592abb4f558402589ac
chenglongwei <weichenglong@gmail.com>
2016-04-12 21:24:34 -0700

    add share_id & extends from object

--- TrackerRepository.py
+++ TrackerRepository.py
@@ -1,7 +1,7 @@
 import redis
 
 
-class TrackerRepository:
+class TrackerRepository(object):
     '''Interact with redis database.'''
 
     def __init__(self):
@@ -11,41 +11,49 @@ class TrackerRepository:
     def report_active(self, ip, port):
         self.r.set(self.get_active_peer_key(ip, port), "active", 5 * 60)
 
-    def report_chunk(self, chunk_id, ip, port):
-        self.r.sadd(self.get_chunk_key(chunk_id), self.get_peer_val(ip, port))
+    def report_chunk(self, share_id, chunk_id, ip, port):
+        self.r.sadd(self.get_chunk_key(share_id, chunk_id), self.get_peer_val(ip, port))
 
-    def get_chunk_peers(self, chunk_id):
-        peers = self.r.smembers(self.get_chunk_key(chunk_id))
-        return filter(lambda peer: self.is_peer_active(peer), peers)
+    def get_chunk_peers(self, share_id, chunk_id):
+        chunk_key = self.get_chunk_key(share_id, chunk_id)
+        legacy_peers = self.r.smembers(chunk_key)
+        active_peers = filter(lambda peer: self.is_peer_active(peer), legacy_peers)
+
+        for dead_peer in legacy_peers - set(active_peers):
+            self.r.srem(chunk_key, dead_peer)
+
+        return active_peers
 
     def is_peer_active(self, peer):
         ip_port = peer.split("#")
-        return self.r.exists(self.get_active_peer_key(ip_port[0], int(ip_port[1])))
+        return self.r.exists(self.get_active_peer_key(ip_port[0], ip_port[1]))
 
-    def get_chunk_key(self, chunk_id):
-        return "chunk#" + chunk_id
+    def get_chunk_key(self, share_id, chunk_id):
+        return "chunk#{share_id}#{chunk_id}".format(share_id=share_id, chunk_id=chunk_id)
 
     def get_peer_val(self, ip, port):
-        return ip + "#" + str(port)
+        return "{ip}#{port}".format(ip=ip, port=port)
 
     def get_active_peer_key(self, ip, port):
-        return "active_peer#" + self.get_peer_val(ip, port)
+        return "active_peer#{peer_val}".format(peer_val=self.get_peer_val(ip, port))
+
 
 repo = TrackerRepository()
 # test setup
 repo.report_active("127.0.0.1", 1234)
 repo.report_active("127.0.0.3", 1234)
-repo.report_chunk("1001", "127.0.0.1", 1234)
-repo.report_chunk("1001", "127.0.0.2", 1234)
-repo.report_chunk("1001", "127.0.0.3", 1234)
+repo.report_chunk("fdef", "1001", "127.0.0.1", 1234)
+repo.report_chunk("fdef", "1001", "127.0.0.2", 1234)
+repo.report_chunk("fdef", "1001", "127.0.0.3", 1234)
 
 # dup
-repo.report_chunk("1001", "127.0.0.1", 1234)
-repo.report_chunk("1001", "127.0.0.2", 1234)
-repo.report_chunk("1001", "127.0.0.3", 1234)
-print repo.get_chunk_peers("1001")
+repo.report_chunk("fdef", "1001", "127.0.0.1", 1234)
+repo.report_chunk("fdef", "1001", "127.0.0.2", 1234)
+repo.report_chunk("fdef", "1001", "127.0.0.3", 1234)
+
+print repo.get_chunk_peers("fdef", "1001")
 
 # test clean up
 repo.r.delete(repo.get_active_peer_key("127.0.0.1", 1234))
 repo.r.delete(repo.get_active_peer_key("127.0.0.3", 1234))
-repo.r.delete(repo.get_chunk_key("1001"))
+repo.r.delete(repo.get_chunk_key("fdef", "1001"))
--- TrackerService.py
+++ TrackerService.py
@@ -1,35 +1,35 @@
 import TrackerRepository
 
 
-class TrackerService:
+class TrackerService(object):
     def __init__(self):
         self.repo = TrackerRepository.TrackerRepository()
 
     def report_active(self, ip, port):
         self.repo.report_active(ip, port)
 
-    def report_chunk(self, chunk_id, ip, port):
-        self.repo.report_chunk(chunk_id, ip, port)
+    def report_chunk(self, share_id, chunk_id, ip, port):
+        self.repo.report_chunk(share_id, chunk_id, ip, port)
 
-    def get_chunk_peers(self, chunk_id):
-        return self.repo.get_chunk_peers(chunk_id)
+    def get_chunk_peers(self, share_id, chunk_id):
+        return self.repo.get_chunk_peers(share_id, chunk_id)
 
 
 trackService = TrackerService()
 # test setup
 trackService.report_active("127.0.0.1", 1234)
 trackService.report_active("127.0.0.3", 1234)
-trackService.report_chunk("1001", "127.0.0.1", 1234)
-trackService.report_chunk("1001", "127.0.0.2", 1234)
-trackService.report_chunk("1001", "127.0.0.3", 1234)
+trackService.report_chunk("fdef", "1001", "127.0.0.1", 1234)
+trackService.report_chunk("fdef", "1001", "127.0.0.2", 1234)
+trackService.report_chunk("fdef", "1001", "127.0.0.3", 1234)
 
 # dup
-trackService.report_chunk("1001", "127.0.0.1", 1234)
-trackService.report_chunk("1001", "127.0.0.2", 1234)
-trackService.report_chunk("1001", "127.0.0.3", 1234)
-print trackService.get_chunk_peers("1001")
+trackService.report_chunk("fdef", "1001", "127.0.0.1", 1234)
+trackService.report_chunk("fdef", "1001", "127.0.0.2", 1234)
+trackService.report_chunk("fdef", "1001", "127.0.0.3", 1234)
+print trackService.get_chunk_peers("fdef", "1001")
 
 # test clean up
 trackService.repo.r.delete(trackService.repo.get_active_peer_key("127.0.0.1", 1234))
 trackService.repo.r.delete(trackService.repo.get_active_peer_key("127.0.0.3", 1234))
-trackService.repo.r.delete(trackService.repo.get_chunk_key("1001"))
+trackService.repo.r.delete(trackService.repo.get_chunk_key("fdef", "1001"))
